a531a84e9138df0a324121ed6586e902a06ce37a
javierlaporte <javilaport@gmail.com>
2022-11-09 20:33:46 -0300

    agrego navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,27 @@
-const NavBar = () => {
-    return <div />;
+const NavBar = () => {    
+    return <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <h1 class="navbar-brand" href="#">The Grow</h1>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Sobre Nosotros</a>
+          </li>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>
+    ;
 };
 
 export default NavBar;
